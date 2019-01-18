50c25017358a6d75d2e43adec77c6197fd8bfa4a
Ali Azmi <aliazmi68@gmail.com>
2019-01-18 22:46:32 +0100

    minor change

--- ttl_generator.py
+++ ttl_generator.py
@@ -1,15 +1,12 @@
-class ttlGenerator:
-    'Class for generating ttl file'
+class TtlGenerator:
+    # Class for generating ttl file
 
     def __init__(self, filename):
         self.filename = filename
-        f = open(filename + '.ttl', "w")
-        f.close()
+        self.f = open(filename + '.ttl', "w")
 
     def addFact(self, factID, truthValue, confidence):
-        f = open(self.filename + '.ttl', "a")
-        f.write('<http://swc2017.aksw.org/task2/dataset/'+str(factID)+'> ' + '<http://swc2017.aksw.org/has'+truthValue+'> ' + '"'+str(confidence)+'"' + '^^<http://www.w3.org/2001/XMLSchema#double> .\n')
-        f.close()
+        self.f.write('<http://swc2017.aksw.org/task2/dataset/'+str(factID)+'> ' + '<http://swc2017.aksw.org/has'+truthValue+'> ' + '"'+str(confidence)+'"' + '^^<http://www.w3.org/2001/XMLSchema#double> .\n')
 
-
-
+    def closeFile(self):
+        self.f.close()
